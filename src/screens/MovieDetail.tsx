0ce998833370e81514e4e002c83bb31d8a00c729
I Kadek Bima Diksatriadi <kadekbimadiksa@gmail.com>
2024-06-27 00:36:30 +0800

    set up environtment variables

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { API_URL, API_ACCESS_TOKEN } from '@env';
 
 const MovieDetail = ({ navigation }: any): any => {
   const fetchData = (): void => {
-    // Ganti dengan API key atau access token yang sesuai
-    const ACCESS_TOKEN =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjkwODJiMWI3NWRlZDI2N2QxYjRjZTc4YWYyNGQ0OSIsIm5iZiI6MTcxOTQxMzI5OC42NzU4MTYsInN1YiI6IjY2N2MyNmViYzM3NzM4ZWJhMDU1N2Q1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tPfI9yZe8bLP9xfIr9ZjYI1kYW7P9GL3GIzMa2GDtp8';
-
-    const url =
-      'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+      throw new Error('ENV not found');
+    }
 
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
       },
     };
 
-    fetch(url, options)
+    fetch(API_URL, options)
       .then(async (response) => await response.json())
       .then((response) => {
         console.log(response);
